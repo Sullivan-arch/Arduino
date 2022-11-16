e4c27ebcdc233e56e0421619d4b2130d8c2b7519
Sullivan A <sullivanabegg8@gmail.com>
2022-11-16 07:44:21 -0500

    Changed builtin LEDs more

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -9,6 +9,7 @@ Program:Blink
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
 }
 
 // the loop function runs over and over again forever
@@ -16,5 +17,7 @@ void loop() {
   digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
+  digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
 }

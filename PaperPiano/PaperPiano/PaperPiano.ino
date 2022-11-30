b12b0d1e1eb66a74a76b9dbb6f23980a4aeac5d1
Sullivan A <sullivanabegg8@gmail.com>
2022-11-30 07:44:47 -0500

    Updated Resources and Background Information

--- PaperPiano/PaperPiano/PaperPiano.ino
+++ PaperPiano/PaperPiano/PaperPiano.ino
@@ -1,3 +1,13 @@
+
+/*
+ * Programer:Sullivan Abegg
+ * Date:11/30/22
+ 
+ * Resource:https://create.arduino.cc/projecthub/Barqunics/paper-piano-with-arduino-e27da7?ref=platform&ref_id=424_trending___&offset=69
+ */
+
+
+
 void setup() {
   // put your setup code here, to run once:
 

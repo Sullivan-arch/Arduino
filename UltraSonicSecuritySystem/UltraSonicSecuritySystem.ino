236fa989778b9227c9818d3e50b1fdd11be4d0b9
Sullivan A <sullivanabegg8@gmail.com>
2022-12-12 07:48:25 -0500

    USS Commit

---
+++ UltraSonicSecuritySystem/UltraSonicSecuritySystem.ino
@@ -0,0 +1,7 @@
+/
+ *Programmer:Sullivan Abegg
+ *Date:12/12/22
+ *Program:Ultrasonic Security System
+ *
+ *Resource:https://create.arduino.cc/projecthub/Krepak/ultrasonic-security-system-3afe13?ref=search&ref_id=red%20green%20yellow%20light%20echo%20sensor&offset=1
+ */
